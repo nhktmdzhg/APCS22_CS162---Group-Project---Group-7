943e58390a1bc052830e8d1fd0458115a8f99791
Demonauuu <118471153+Demonauuu@users.noreply.github.com>
2023-03-20 11:43:29 +0700

    change password and build menu

---
+++ Menu.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include "Login.h"
+
+using namespace std;
+
+void Profile(User_node *&current_user);
+
+void Grade(User_node *&current_user);
+
+void Preferences(User_node *&current_user);
+
+void Courses(User_node *&current_user);
+
