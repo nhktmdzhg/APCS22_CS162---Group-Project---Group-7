d4c9fa17d1969ba76d56f88a3ada78823e7d23f8
To Ngoc Hung <116636558+kasksa1902@users.noreply.github.com>
2023-05-01 08:35:44 +0700

    cau21

---
+++ cau21.cpp
@@ -0,0 +1,5 @@
+#include"AllStruct.h"
+
+void ViewTheScoreboardOfCourse(course_node *&courses, string course_id){
+
+}
---
+++ cau22.cpp
@@ -0,0 +1,5 @@
+#include"AllStruct.h"
+
+void updateStudentResult(string student_id, string course_id, course_node *&courses){
+    
+}
