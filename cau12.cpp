25ba366ca8184f1e96e8d45d7789b184c2d8ac22
nguyen2192004 <phamgianguyen.qn@gmail.com>
2023-05-03 22:55:25 +0700

    sua lai dinh dang tu int sang string
    fix code

--- cau12.cpp
+++ cau12.cpp
@@ -1,5 +1,5 @@
 #include "AllStruct.h"
-void removeStudentFromCourse(course_node *courses, int course_id, int student_id) {
+/*void removeStudentFromCourse(course_node *courses, int course_id, int student_id) {
     // Find the course with the given ID by looping through the whole list
     course_node *current_course = courses;
     while (current_course != nullptr && current_course->data.course_id != course_id) 
@@ -36,5 +36,46 @@ void removeStudentFromCourse(course_node *courses, int course_id, int student_id
     }
     delete current_student;
     current_course->data.max_student++;
+}*/
+void removeStudentFromCourse(course_node* courses, string course_id, string student_id) 
+{
+    // Find the course
+    course_node* course_ptr = courses;
+    while (course_ptr != nullptr && course_ptr->data.course_id != course_id) {
+        course_ptr = course_ptr->next;
+    }
+    if (course_ptr == nullptr) {
+        cout << "Error: Cannot find the course with id " << course_id << endl;
+        return;
+    }
+
+    // Find the student in the course
+    student_node* prev = nullptr;
+    student_node* current = course_ptr->data.head;
+    // keeping track of the prev and next student inorder to delete
+    while (current != nullptr && current->data.ID != student_id) 
+	{
+        prev = current;
+        current = current->next;
+    }
+    if (current == nullptr) 
+	{
+        cout << "Error: Cannot find the student with id " << student_id << " in the course " << course_ptr->data.course_name << endl;
+        return;
+    }
+
+    // Remove the student from the course
+    if (prev == nullptr) 
+	{
+    	
+        // The first student is the one to be removed
+        course_ptr->data.head = current->next;
+    } else 
+	{
+        prev->next = current->next;
+    }
+    delete current;
+
+    cout << "Successfully removed student with id " << student_id << " from the course " << course_ptr->data.course_name << endl;
 }
 
